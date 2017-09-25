8386224d53d520ac388402e1f2dff2adf0e113f7
aungkonazim <aungkonazim@gmail.com>
2017-09-24 21:38:29 -0500

    code saved

--- assignment/proportion/proportion.R
+++ assignment/proportion/proportion.R
@@ -1,1 +1,5 @@
-prop.test(x=c(135,47),n=c(1781,1443),alternative = c("greater"),correct=F)
+prop.test(x=c(47,135),n=c(1443,1781),alternative ="two.sided",correct=F)
+prop.test(x=c(18,5),n=c(171+18,99+5),correct=F)
+fisher.test(matrix(c(18,171,5,99),ncol=2))
+
+prop.test(x=c(18+171,5+99),n=c(171+18+10845,99+5+10993),correct=F)
